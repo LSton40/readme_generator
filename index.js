84590af8db5d3365bffdc66004f7e5babe3947aa
LSton40 <logan.sutton@gmail.com>
2022-07-17 17:37:20 -0500

    All but successful function passes

---
+++ index.js
@@ -0,0 +1,92 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
+const readme = 'README.md';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please give your project a title'
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the name you would like to be credited as author of this project'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your preferred contact email for this project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose your license',
+        choices: ['MIT License', 'Apache License 2.0', 'Mozilla Public License 2.0', 'GNU General Public License v3.0', 'GNU Affero General Public License v3.0', 'GNU Lesser General Public License v3.0', 'ISC License', 'Boost Software License 1.0', 'The Unlicense', 'No license']
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please describe the installation for your project'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please describe how to use your project'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please describe information on contributing to your project'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please suggest some tests for your project'
+    }
+];
+
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+function writeToFile('README.md', data, (error) => {
+    error ? console.log(error) : console.log('Success!')
+});
+
+
+// fs.writeFile('log.txt', 'new line of text', (err) =>
+// err ? console.error(err) : console.log('Success!')
+// );
+
+// fs.appendFile('log.txt', 'uptown girl', (err)=>
+//   err ? console.error(err) : console.log('Success!')
+// );
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        markdown.generateMarkdown(data);
+    })
+    .then((output) => {
+        fs[writeToFile(output)];
+    })
+};
+
+// Function call to initialize app
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  var licenseBadge;
   switch(license) {
-    var licenseBadge;
-    case 'MIT':
+    case 'MIT License':
       licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
       break;
-    case 'Apache':
+    case 'Apache License 2.0':
       licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
       break;
-    case 'Mozilla':
+    case 'Mozilla Public License 2.0':
       licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
       break;
-    case 'GNU GPL v3.0':
+    case 'GNU General Public License v3.0':
       licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
       break;
-    case 'GNU AGPL v3.0':
+    case 'GNU Affero General Public License v3.0':
       licenseBadge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
       break;
-    case 'GNU LGPL v3.0':
+    case 'GNU Lesser General Public License v3.0':
       licenseBadge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
       break;
-    case 'ISC':
+    case 'ISC License':
       licenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
       break;
-    case 'Boost':
+    case 'Boost Software License 1.0':
       licenseBadge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
       break;
-    case 'Unlicense':
+    case 'The Unlicense':
       licenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
       break;
     default:
       licenseBadge = '';
   }
-  // renderLicenseLink();
-  // renderLicenseSection();
+  console.log(licenseBadge);
   return licenseBadge;
 }
 
@@ -43,65 +42,92 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   var licenseLink;
   switch(license) {
-    case 'MIT':
+    case 'MIT License':
       licenseLink = '[MIT License](https://spdx.org/licenses/MIT.html)';
       break;
-    case 'Apache':
+    case 'Apache License 2.0':
       licenseLink = '[Apache License 2.0](https://spdx.org/licenses/Apache-2.0.html)';
       break;
-    case 'Mozilla':
+    case 'Mozilla Public License 2.0':
       licenseLink = '[Mozilla License 2.0](https://spdx.org/licenses/MPL-2.0.html)';
       break;
-    case 'GNU GPL v3.0':
+    case 'GNU General Public License v3.0':
       licenseLink = '[GNU General Public License v3.0](https://spdx.org/licenses/GPL-3.0-or-later.html)';
       break;
-    case 'GNU AGPL v3.0':
+    case 'GNU Affero General Public License v3.0':
       licenseLink = '[GNU Affero General Public License v3.0](https://spdx.org/licenses/AGPL-3.0-or-later.html)';
       break;
-    case 'GNU LGPL v3.0':
+    case 'GNU Lesser General Public License v3.0':
       licenseLink = '[GNU Lesser General Public License v3.0](https://spdx.org/licenses/LGPL-3.0-or-later.html)';
       break;
-    case 'ISC':
+    case 'ISC License':
       licenseLink = '[ISC License](https://spdx.org/licenses/ISC.html)';
       break;
-    case 'Boost':
+    case 'Boost Software License 1.0':
       licenseLink = '[Boost License 1.0](https://spdx.org/licenses/BSL-1.0.html)';
       break;
-    case 'Unlicense':
+    case 'The Unlicense':
       licenseLink = '[The Unlicense](https://spdx.org/licenses/Unlicense.html)';
       break;
     default:
       licenseLink = '';
   }
-  // renderLicenseSection();
-
+  console.log(licenseLink);
     return licenseLink;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  renderLicenseBadge(license);
+  // renderLicenseBadge(license);
   renderLicenseLink(license);
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-    ## Description ${data.description}
-    ## Table of Contents
-    1. [Installation](##installation)
-    2. [Usage](##usage)
-    3. [License](##license)
-    4. [How to Contribute](##how-to-contribute)
-    5. [Tests](##tests)
-    6. [Questions](##questions)
-    ## Installation ${data.installation}
-    ## Usage ${data.usage}
-    ## License ${renderLicenseSection(data.license)}
-    ## How to Contribute ${data.contribute}
-    ## Tests ${data.tests}
-    ## Questions ${data.github} ${data.email}
+  return `# ${data.title}  
+
+    By: ${data.name}  
+
+    ${renderLicenseBadge(data.license)}  
+
+    ## Description  
+    
+    ${data.description}  
+
+    ## Table of Contents  
+
+    - [Installation](##installation)  
+    - [Usage](##usage)  
+    - [License](##license)  
+    - [How to Contribute](##how-to-contribute)  
+    - [Tests](##tests)  
+    - [Questions](##questions)  
+
+    ## Installation  
+    
+    ${data.installation}  
+
+    ## Usage  
+    ${data.usage}  
+
+    ## License  
+    
+    ${renderLicenseSection(data.license)}  
+
+    ## How to Contribute  
+    
+    ${data.contribute}  
+
+    ## Tests  
+    
+    ${data.tests}  
+
+    ## Questions  
+    
+    [${data.github} GitHub](https://github.com/${data.github})  
+
+    If you have any questions, please contact me at ${data.email}.
 `;
 }
 
